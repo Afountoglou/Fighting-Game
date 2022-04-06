0a90331333b702ff82997ae840eae9d35eb354bc
Thanasis Fountoglou <thanasesphountoglou@gmail.com>
2022-04-06 21:53:24 +0300

    Small changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d');
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
 
 canvas.width = 1024
 canvas.height = 576
@@ -7,9 +7,52 @@ canvas.height = 576
 c.fillRect(0, 0, canvas.width, canvas.height)
 
 class Sprite {
-    constructor(position) {
+    constructor({position, velocity}) {
         this.position = position
+        this.velocity = this.velocity
+    }
+
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, 50, 150 )
+    }
+
+    update() {
+        this.draw()
+        this.position.y += 10
     }
 }
 
-const player = new Sprite()
+const player = new Sprite({
+    position: {
+    x:0,
+    y:0
+  },
+  velocity: {
+      x: 0,
+      y: 0
+  }
+})
+
+
+const enemy = new Sprite({
+    position: {
+    x:400,
+    y:100
+  },
+  velocity: {
+      x: 0,
+      y: 0
+  }
+})
+
+
+console.log(player)
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    player.update()
+    enemy.update()
+}
+
+animate()
