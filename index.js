ae7eb9ec9614c7d588762f7cf1b4ea2a66cff2ad
Afountoglou <thanasesphountoglou@gmail.com>
2022-04-12 09:30:17 +0300

    Healthbars Done

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ class Sprite {
         }
         this.color = color
         this.isAttacking
+        this.health =  100
     }
 
     draw() {
@@ -165,7 +166,8 @@ function animate() {
         })
          && player.isAttacking) {
         player.isAttacking = false
-        console.log("x")
+        enemy.health -= 20
+        document.querySelector('#enemyHealth').style.width = enemy.health + '%'
     }
     
     if ( 
@@ -175,7 +177,8 @@ function animate() {
         })
          && enemy.isAttacking) {
             enemy.isAttacking = false
-        console.log("c")
+            player.health -= 20
+            document.querySelector('#playerHealth').style.width = player.health + '%'
     }
 }
 
